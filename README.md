47aea8be5568649712a952ce0c978775f350862e
David Sayre <dsayre@teamallegiance.com>
2022-12-17 00:04:03 -0500

    LandingPage/SearchField.php

---
+++ README.md
@@ -0,0 +1,21 @@
+### Code snippets from playing with Ibexa DXP 4
+
+
+### Snippet: Indexing LandingPage field type
+
+1. Copy or merge the config/*.yaml into your project
+1. Copy the ./src files into your project's ./src
+1. Edit the Content Type with an ezlandingpage and check the 'Searchable'
+   2. If that doesn't work, I you can use a SQL query
+   
+    update ezcontentclass_attribute set is_searchable = 1 where data_type_string = 'ezlandingpage';
+
+1. Test
+
+* creat a content page with Page Builder
+* add a RichText block with some unique text (example 'instantpot')
+* publish
+* type your RichText block text (example 'instantpot) into the (Admin) site search 
+* you should find content matches.
+
+ 
---
+++ ezlandingpage-indexable/src/Ibexa/Core/FieldTypes/LandingPage/SearchField.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Author @ David Sayre
+ * Repo: https://github.com/davidsayre/ibexa-dxp-snippets
+ */
+namespace App\Ibexa\Core\FieldType\LandingPage;
+
+use Ibexa\Contracts\Core\FieldType\Indexable;
+use Ibexa\Contracts\Core\Persistence\Content\Field;
+use Ibexa\Contracts\Core\Persistence\Content\Type\FieldDefinition;
+use Ibexa\Contracts\Core\Search;
+
+/**
+ * Indexable definition for LandingPage field type.
+ */
+class SearchField implements Indexable
+{
+    public function getIndexData(Field $field, FieldDefinition $fieldDefinition)
+    {
+        $blockText = $this->extractZoneBlockAttributeText($field->value->externalData);
+
+        return [
+            new Search\Field(
+                'block_text',
+                $blockText,
+                new Search\FieldType\StringField()
+            ),
+            new Search\Field(
+                'block_text',
+                $blockText,
+                new Search\FieldType\FullTextField()
+            ),
+        ];
+    }
+
+    public function getIndexDefinition()
+    {
+        return [
+            'block_text' => new Search\FieldType\StringField(),
+        ];
+    }
+
+    /**
+     * Get name of the default field to be used for matching.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for matching. Default field is typically used by Field criterion.
+     *
+     * @return string
+     */
+    public function getDefaultMatchField()
+    {
+        return 'block_text';
+    }
+
+    /**
+     * Get name of the default field to be used for sorting.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for sorting. Default field is typically used by Field sort clause.
+     *
+     * @return string
+     */
+    public function getDefaultSortField()
+    {
+        return 'block_text';
+    }
+
+    /**
+     * Simple loop over all zones, blocks, attributes searching for richtext <?xml> and extracting usable text into simple string
+     * This is written to be very careful of deep arrays because the structures may change
+     * @param $externalData
+     * @return string
+     */
+    private function extractZoneBlockAttributeText($externalData) {
+        $blockValues = array();
+        if(is_array($externalData) && array_key_exists('zones', $externalData)) {
+            foreach ($externalData['zones'] as $zone) {
+                if (is_array($zone) && array_key_exists('blocks',$zone)) {
+                    foreach ($zone['blocks'] as $block) {
+                        if (is_array($block) && array_key_exists('attributes',$block)) {
+                            foreach($block['attributes'] as $attribute) {
+                                if(array_key_exists('value',$attribute)) {
+                                    $value = $attribute['value'];
+                                    // look for richtext and extract the simple strings
+                                    // could detect and extract other block attribute values in the future
+                                    // we don't want csv, doubles, integers, content_id, location_id etc..
+                                    if(stripos($value,'<?xml') !== false) {
+                                        $blockValues[] = strip_tags($value);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // convert to simple " " delim string
+        return implode(" ",$blockValues);
+    }
+}
