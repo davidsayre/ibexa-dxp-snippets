795c4c52765573aa936a00262600acc6c6609d99
David Sayre <dsayre@teamallegiance.com>
2023-05-10 23:45:13 -0400

    EmptyTrashCommand --save

--- command-empty-trash/src/Command/EmptyTrashCommand.php
+++ command-empty-trash/src/Command/EmptyTrashCommand.php
@@ -9,16 +9,13 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use eZ\Publish\Core\Repository\Values\Content\TrashItem;
-use Ibexa\AdminUi\Form\Data\Trash\TrashEmptyData;
 use Ibexa\Contracts\Core\Repository\Values\Content\Query;
+use Ibexa\Contracts\Core\Repository\Values\Content\Trash\SearchResult;
 use Ibexa\Core\Repository\Repository;
-use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Ibexa\Contracts\Core\Repository\Values\Content\Trash\SearchResult;
 
 
 class EmptyTrashCommand extends Command
@@ -38,20 +35,19 @@ class EmptyTrashCommand extends Command
         $this
             ->setName(self::COMMAND_NAME)
             ->setDescription('Empty trash')
-            ->addOption(
-                'limit',
-                'l',
-                InputOption::VALUE_OPTIONAL,
-                'Item limit',
-                100
-            )
+            ->addOption('limit', '--limit', InputOption::VALUE_OPTIONAL, 'number of items to process: default 10', 10)
+            ->addOption('save', '--save', InputOption::VALUE_OPTIONAL, '--save 1 will make real DB and File changes; else dry run logging', 0);
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $limit = (int) $input->getOption('limit');
-
+        // save / dry run
+        $save = false;
+        if ($input->getOption('save') == "1" | $input->getOption('save') == true) {
+            $save = true;
+        }
         $output->writeln('Running ..');
         $output->writeln('');
 
@@ -64,15 +60,36 @@ class EmptyTrashCommand extends Command
                 return $this->repository->getTrashService()->findTrashItems($query);
             }
         );
+        $totalProcessed = 0;
+        $totalDeleted = 0;
+        $totalDryRun = 0;
         foreach ($trashItems->items as $item) {
+            $totalProcessed++;
             $content = $item->getContent();
-            echo "".$item->id." ".$content->getName()."\n";
-            $this->repository->sudo(
-                function () use ($item) {
-                    return $this->repository->getTrashService()->deleteTrashItem($item);
+            $output->write( "content [".$content->id."] ".$content->getName()." "); // no line break, space
+            if($save === true) {
+                try {
+                    $this->repository->sudo(
+                        function () use ($item) {
+                            return $this->repository->getTrashService()->deleteTrashItem($item);
+                        }
+                    );
+                    $totalDeleted++;
+                    $output->writeln("[removed]"); // line break
+                } catch (\Exception $e) {
+                    $output->writeln("ERROR .. "); // line break
+                    echo "File: ".$e->getFile()."\n";
+                    echo "Line: ".$e->getLine()."\n";
+                    echo "Message: ".$e->getMessage()."\n";
                 }
-            );
+            } else {
+                $totalDryRun++;
+                $output->writeln("[Dry Run]"); //line break
+            }
         }
+        $output->writeln("Total processed: ".$totalProcessed);
+        $output->writeln("Total deleted: ".$totalDeleted);
+        $output->writeln("Total dry run: ".$totalDryRun);
 
         return Command::SUCCESS;
 
