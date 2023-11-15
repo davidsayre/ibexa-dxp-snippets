ac365354e86868349cc593c51cc87d3c678c10c1
Fabian Gassen <gassen@salient-doremus.de>
2023-11-15 11:43:31 +0100

    bugfix checkValueType
    checkValueType expected the ValueType to be in the app namespace / fixes translations

--- ezlandingpage-indexable/src/Ibexa/FieldTypePage/FieldType/LandingPage/Type.php
+++ ezlandingpage-indexable/src/Ibexa/FieldTypePage/FieldType/LandingPage/Type.php
@@ -3,8 +3,17 @@
 namespace App\Ibexa\FieldTypePage\FieldType\LandingPage;
 
 
+use Ibexa\Core\Base\Exceptions\InvalidArgumentType;
+
 class Type extends \Ibexa\FieldTypePage\FieldType\LandingPage\Type
 {
+    protected static function checkValueType($value): void
+    {
+        if (!$value instanceof \Ibexa\FieldTypePage\FieldType\LandingPage\Value) {
+            throw new InvalidArgumentType('$value', '\Ibexa\FieldTypePage\FieldType\LandingPage\Value', $value);
+        }
+    }
+
     public function isSearchable(): bool
     {
         return true;
