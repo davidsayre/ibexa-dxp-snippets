4ff142e0ae4ea8e0c30e3b2caf977668ead26cb2
David Sayre <dsayre@teamallegiance.com>
2024-11-01 11:09:28 -0400

    promos

---
+++ promos/src/Helpers/PromoMonthlyPickerHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Helpers;
+
+use DateTime;
+
+class PromoMonthlyPickerHelper {
+    /**
+     * Given a specific date, pick from the pool of promos
+     * Days of the month take effect is > 1 promo found
+     * @param $promos
+     * @param $date
+     * @return mixed|null
+     */
+    public function pickMonthlyPromoByDate($promos, $date = null)
+    {
+
+        if (!is_a($date, DateTime::class)) {
+            $date = new DateTime();
+        }
+
+        // Monthly Promos
+        $dayNumber = $date->format('d');
+        $numPromos = count($promos);
+
+        $lastDom = new DateTime('last day of this month');
+        $numDaysOfCurrentMonth = $lastDom->format('d');
+
+        // single vs multiple calculations by day of month
+        if (is_array($promos) && !empty($promos)) {
+            if (count($promos) == 1) {
+                return $promos[0];
+            } else {
+                $pos = floor(($dayNumber / $numDaysOfCurrentMonth) * $numPromos); // zero based array
+                // exact day edge case
+                if ($pos > count($promos) - 1) {
+                    $pos = $pos - 1;
+                }
+                return $promos[$pos];
+            }
+        }
+        return null;
+    }
+}
