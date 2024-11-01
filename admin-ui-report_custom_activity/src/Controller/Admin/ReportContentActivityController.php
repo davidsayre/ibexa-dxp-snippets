cf469bf82108d6801253b38c1d747c4f454f6d9e
David Sayre <dsayre@teamallegiance.com>
2024-10-31 23:38:56 -0400

    admin-ui-report_custom_activity/src/Controller/Admin/ReportContentActivityController.php

--- admin-ui-report_custom_activity/src/Controller/Admin/ReportContentActivityController.php
+++ admin-ui-report_custom_activity/src/Controller/Admin/ReportContentActivityController.php
@@ -2,62 +2,55 @@
 
 declare(strict_types=1);
 
+namespace App\Controller\Admin;
 
-use App\Form\Admin\Reports\ReportContentActivityType;
 use App\Model\Admin\Reports\ReportContentActivityParams;
 use App\Services\Admin\Reports\ReportContentActivityService;
 use App\Services\Admin\Reports\ReportContentExportService;
 use Cocur\Slugify\Slugify;
-use GuzzleHttp\Stream\Stream;
-use Ibexa\Bundle\IO\BinaryStreamResponse;
 use Ibexa\Contracts\Core\Repository\SearchService;
 use Ibexa\Contracts\Core\Repository\Values\Content\Language;
-use Ibexa\Core\MVC\Symfony\Controller\Content\DownloadController;
 use Ibexa\Core\Pagination\Pagerfanta\LocationSearchAdapter;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\View\TwitterBootstrap3View;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\SubmitButton;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use App\Form\Admin\Reports\ReportContentActivityType;
 
-class ReportContentActivityController extends AbstractController {
-
-
+class ReportContentActivityController extends AbstractController
+{
     const XML_DOWNLOAD_LANGUAGES_PARAM = 'languages'; // see xml download link in twig
     const MAX_RESULTS = 25;
-
     protected ReportContentActivityService $reportContentActivityService;
     protected ReportContentExportService $reportContentExportService;
     protected SearchService $searchService;
 
     public function __construct(
         ReportContentActivityService $reportContentActivityService,
-        ReportContentExportService $reportContentExportService,
-        SearchService $searchService
-    ) {
+        ReportContentExportService   $reportContentExportService,
+        SearchService                $searchService
+    )
+    {
         $this->reportContentActivityService = $reportContentActivityService;
         $this->reportContentExportService = $reportContentExportService;
         $this->searchService = $searchService;
     }
 
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
 
         $errors = [];
         $limit = $request->query->get('limit', self::MAX_RESULTS);
-        $onPage = $request->query->get('page', 1);
-        $offset = $request->query->get("offset") ?: 0;
+        $page = $request->query->get('page', 1);
 
         // defaults
         $emptyData = [
-            'section_id'=>8
+            'section_id' => 8
         ];
 
-        $form = $this->createForm(ReportContentActivityType::class,$emptyData,['method' => 'GET']);
+        $form = $this->createForm(ReportContentActivityType::class, $emptyData, ['method' => 'GET']);
 
         $form->handleRequest($request);
 
@@ -65,32 +58,32 @@ class ReportContentActivityController extends AbstractController {
         $params['form'] = $form->createView();
         $params['xmlDownloadLanguageCsv'] = ""; // default empty
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $formData = $form->getData();
 
             // extract language filter
             $languages = Language::ALL;
-            if(array_key_exists(ReportContentActivityType::LANGUAGE_CODE_FIELD,$formData) && !empty($formData[ReportContentActivityType::LANGUAGE_CODE_FIELD])) {
+            if (array_key_exists(ReportContentActivityType::LANGUAGE_CODE_FIELD, $formData) && !empty($formData[ReportContentActivityType::LANGUAGE_CODE_FIELD])) {
                 $languages = [$formData[ReportContentActivityType::LANGUAGE_CODE_FIELD]]; // example: [eng-US] used by xml exporter
             }
 
             // Now pass the languages csv into the template for the 'xml' links
-            $params['xmlDownloadLanguageCsv'] = implode(",",$languages);
+            $params['xmlDownloadLanguageCsv'] = implode(",", $languages);
 
 
             $searchParams = $this->paramFill($formData);
             $query = $this->reportContentActivityService->buildLocationQuery($searchParams);
-            if(is_object($query)) {
+            if (is_object($query)) {
 
                 /** @var SubmitButton $clicked */
                 $clicked = $form->getClickedButton();
                 $buttonName = 'unknown';
-                if(is_object($clicked)) {
+                if (is_object($clicked)) {
                     $buttonName = $clicked->getName();
                 }
 
-                if($buttonName === ReportContentActivityType::BUTTON_DOWNLOAD_XML) {
+                if ($buttonName === ReportContentActivityType::BUTTON_DOWNLOAD_XML) {
                     // download XML rows from query
                     $filename = 'content_activity_report_results.xml';
                     $content = $this->reportContentExportService->generateRestXMLRowsByQuery($query, $languages, 1000);
@@ -103,13 +96,13 @@ class ReportContentActivityController extends AbstractController {
                     $response->headers->set('Expires', '0');
                     $response->headers->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0');
                     $response->headers->set('Content-Type', $mimeType);
-                    $response->headers->set( 'Content-disposition', sprintf('filename=%s',$filename));
+                    $response->headers->set('Content-disposition', sprintf('filename=%s', $filename));
                     $response->setContent($content);
                     //$response->setCallback(function () use ($xmlRowText) {echo $xmlRowText;flush();return true;});
                     return $response;
                 }
 
-                if($buttonName === ReportContentActivityType::BUTTON_DOWNLOAD_CSV) {
+                if ($buttonName === ReportContentActivityType::BUTTON_DOWNLOAD_CSV) {
                     $viewAddAttributes = ['language'];
                     $fieldAddAttributes = [];
                     // download XML rows from query
@@ -125,7 +118,7 @@ class ReportContentActivityController extends AbstractController {
                     $response->headers->set('Expires', '0');
                     $response->headers->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0');
                     $response->headers->set('Content-Type', $mimeType);
-                    $response->headers->set( 'Content-disposition', sprintf('filename=%s',$filename));
+                    $response->headers->set('Content-disposition', sprintf('filename=%s', $filename));
                     $response->setContent($content);
                     return $response;
                 }
@@ -138,7 +131,7 @@ class ReportContentActivityController extends AbstractController {
                         $this->searchService
                     ),
                     $request,
-                    $offset,
+                    $page,
                     $limit
                 );
                 // COPY from holder
@@ -151,12 +144,12 @@ class ReportContentActivityController extends AbstractController {
             }
         }
 
-        if(!empty($errors)) {
+        if (!empty($errors)) {
             $params['errors'] = $errors;
         } // if errors
 
         return $this->render(
-            "@standard/admin/reports/content_activity/dashboard.html.twig",
+            "@admin/reports/content_activity/dashboard.html.twig",
             $params
         );
     }
@@ -165,36 +158,37 @@ class ReportContentActivityController extends AbstractController {
      * @param Request $request
      * @return Response
      */
-    public function downloadXmlContent(Request $request) {
+    public function downloadXmlContent(Request $request)
+    {
 
         $contentId = 0;
-        if($request->query->has('contentId')) {
+        if ($request->query->has('contentId')) {
             $contentId = $request->query->get('contentId');
             $contentId = intval($contentId);
         }
 
-        if($request->query->has(self::XML_DOWNLOAD_LANGUAGES_PARAM)) {
-            $languages = explode(",",$request->query->get(self::XML_DOWNLOAD_LANGUAGES_PARAM));
+        if ($request->query->has(self::XML_DOWNLOAD_LANGUAGES_PARAM)) {
+            $languages = explode(",", $request->query->get(self::XML_DOWNLOAD_LANGUAGES_PARAM));
         }
 
-        if(!empty($contentId) && $contentId > 0) {
+        if (!empty($contentId) && $contentId > 0) {
             try {
                 $restContent = $this->reportContentExportService->generateRestXMLByContentId($contentId, $languages);
                 $xmlContent = $this->reportContentExportService->convertRestContentToRestXml($restContent);
 
-                $languageCsvSlug = implode("_",$languages);
+                $languageCsvSlug = implode("_", $languages);
                 $title = $restContent->contentInfo->name;
                 $version = $restContent->contentInfo->currentVersionNo;
                 $slugify = new Slugify();
                 $slugTitle = $slugify->slugify($title);
-                $filename = "content_".$languageCsvSlug."-".$contentId."_".$version."-".$slugTitle.".xml";
+                $filename = "content_" . $languageCsvSlug . "-" . $contentId . "_" . $version . "-" . $slugTitle . ".xml";
 
                 // Create a download response using disposition and file
                 $response = new Response(
-                    $xmlContent,200,
+                    $xmlContent, 200,
                     [
                         'Content-type' => 'application/xml',
-                        'Content-Disposition'=>'attachment; filename="'.$filename.'"'
+                        'Content-Disposition' => 'attachment; filename="' . $filename . '"'
                     ]
                 );
                 return $response;
@@ -206,42 +200,43 @@ class ReportContentActivityController extends AbstractController {
     }
 
     // form param processing here
-    protected function paramFill($formData){
+    protected function paramFill($formData)
+    {
 
         $searchParams = new ReportContentActivityParams();
 
-        if(array_key_exists('date_field',$formData) && !empty($formData['date_field'])) {
+        if (array_key_exists('date_field', $formData) && !empty($formData['date_field'])) {
             $searchParams->setDateField($formData['date_field']);
         }
-        if(array_key_exists('date_from',$formData) && !empty($formData['date_from'])) {
+        if (array_key_exists('date_from', $formData) && !empty($formData['date_from'])) {
             $searchParams->setDateFrom($formData['date_from']);
         }
-        if(array_key_exists('date_to',$formData) && !empty($formData['date_to'])) {
+        if (array_key_exists('date_to', $formData) && !empty($formData['date_to'])) {
             $searchParams->setDateTo($formData['date_to']);
         }
-        if(array_key_exists('section_id',$formData) && !empty($formData['section_id'])) {
+        if (array_key_exists('section_id', $formData) && !empty($formData['section_id'])) {
             $searchParams->setSectionId($formData['section_id']);
         }
-        if(array_key_exists('exclude_location_ids',$formData) && !empty($formData['exclude_location_ids'])) {
+        if (array_key_exists('exclude_location_ids', $formData) && !empty($formData['exclude_location_ids'])) {
             $searchParams->setExcludeLocationIds($formData['exclude_location_ids']);
         }
-        if(array_key_exists('content_type',$formData) && !empty($formData['content_type'])) {
+        if (array_key_exists('content_type', $formData) && !empty($formData['content_type'])) {
             $searchParams->setContentType($formData['content_type']);
         }
-        if(array_key_exists('search_text',$formData) && !empty($formData['search_text'])) {
+        if (array_key_exists('search_text', $formData) && !empty($formData['search_text'])) {
             $searchParams->setSearchText($formData['search_text']);
         }
-        if(array_key_exists('title',$formData) && !empty($formData['title'])) {
+        if (array_key_exists('title', $formData) && !empty($formData['title'])) {
             $searchParams->setTitle($formData['title']);
         }
-        if(array_key_exists('tags',$formData) && is_numeric($formData['tags'])) {
+        if (array_key_exists('tags', $formData) && is_numeric($formData['tags'])) {
             $searchParams->setTagIds([$formData['tags']]);// turn into array
         }
-        if(array_key_exists('language_code',$formData) && !empty($formData['language_code'])) {
+        if (array_key_exists('language_code', $formData) && !empty($formData['language_code'])) {
             $searchParams->setLanguageCode($formData['language_code']);
         }
         //$searchParams->setViewLanguage($formData['view_language']);
-        if(array_key_exists('sort_dir',$formData) && !empty($formData['sort_dir'])) {
+        if (array_key_exists('sort_dir', $formData) && !empty($formData['sort_dir'])) {
             $searchParams->setSortDir($formData['sort_dir']);
         }
 
@@ -254,16 +249,17 @@ class ReportContentActivityController extends AbstractController {
      * @param array $searchResults
      * @return array
      */
-    protected function buildPagerfantaHolder($adapter, Request $request, $offset = 0, $limit = self::MAX_RESULTS)    {
+    protected function buildPagerfantaHolder($adapter, Request $request, $page = 0, $limit = self::MAX_RESULTS)
+    {
 
         // hard limit
-        if($limit > self::MAX_RESULTS) {
+        if ($limit > self::MAX_RESULTS) {
             $limit = self::MAX_RESULTS;
         }
 
         $pagerfanta = new Pagerfanta($adapter);
         $pagerfanta->setMaxPerPage($limit);
-        $pagerfanta->setCurrentPage( $limit > 0 ? ($offset / $limit) + 1 : 1 );
+        $pagerfanta->setCurrentPage($page ? $page : 1);
 
         $paginationView = new TwitterBootstrap3View();
         $paginationOptions = array(
@@ -272,7 +268,7 @@ class ReportContentActivityController extends AbstractController {
             'next_message' => "»"
         );
 
-        $paginationRouteGenerator = function($page) use ($request, $limit) {
+        $paginationRouteGenerator = function ($page) use ($request, $limit) {
             $queryStr = http_build_query(
                 array_merge(
                     $request->query->all(),
