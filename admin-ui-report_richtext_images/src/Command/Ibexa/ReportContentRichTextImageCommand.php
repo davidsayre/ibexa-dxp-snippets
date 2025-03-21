ec3dbc06d6673890ec96f33b446c7f15c77e1fb5
David Sayre <dsayre@teamallegiance.com>
2025-03-21 09:48:43 -0400

    admin-ui-report_richtext_images/src/Command/Ibexa/ReportContentRichTextImageCommand.php

--- admin-ui-report_richtext_images/src/Command/Ibexa/ReportContentRichTextImageCommand.php
+++ admin-ui-report_richtext_images/src/Command/Ibexa/ReportContentRichTextImageCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RichTextImageReportGenerateCommand extends Command
+class ReportContentRichTextImageCommand extends Command
 {
 
     private ReportContentRichTextImageService $reportContentRichTextImageService;
