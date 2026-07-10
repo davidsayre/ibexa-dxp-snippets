43ccfdc499590eba93d889a8de94192361274cad
David Sayre <dsayre@teamallegiance.com>
2026-07-10 15:51:26 -0400

    src/Event/Subscriber/PublishVersion/NavItemOnPublishVersionSubscriber.php

--- src/Event/Subscriber/PublishVersion/NavItemOnPublishVersionSubscriber.php
+++ src/Event/Subscriber/PublishVersion/NavItemOnPublishVersionSubscriber.php
@@ -5,12 +5,22 @@ namespace App\Event\Subscriber\PublishVersion;
 
 use Ibexa\Contracts\Core\Repository\Events\Content\PublishVersionEvent;
 use Ibexa\Contracts\HttpCache\Handler\ContentTagInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Ibexa\Contracts\HttpCache\PurgeClient\PurgeClientInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/* NOTE: must add src/config/services.yaml entry to inject $purgeClient */
 
 class NavItemOnPublishVersionSubscriber implements EventSubscriberInterface
 {
     const CONTENT_TYPE_IDENTIFIER = 'nav_item';
+
+    protected $purgeClient;
+
+    public function __construct(
+        // PurgeClientInterface $purgeClient
+    ) {
+        // $this->purgeClient = $purgeClient;
+    }
 
     public static function getSubscribedEvents(): array
     {
@@ -19,14 +29,18 @@ class NavItemOnPublishVersionSubscriber implements EventSubscriberInterface
         ];
     }
 
-    public function onPublishVersionEvent(PublishVersionEvent $event, PurgeClientInterface $purgeClient): void
+    public function onPublishVersionEvent(PublishVersionEvent $event): void
     {
         $content = $event->getContent();
         // purge all pages tagged with content type matching 'nav_item'
         $contentType = $content->getContentType();
         if ($contentType->identifier === self::CONTENT_TYPE_IDENTIFIER) {
-            $tags = [ContentTagInterface::CONTENT_TYPE_PREFIX . $contentType->id];
-            $purgeClient->purge($tags);
+            try {
+                $tags = [ContentTagInterface::CONTENT_TYPE_PREFIX . $contentType->id];
+                // $this->purgeClient->purge($tags);
+            } catch(\Exception $e) {
+                // do nothing
+            }
         }
     }
 
